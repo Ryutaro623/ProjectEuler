755478af3e465d821b2986e9c7a3fb7dd717693a
Ryutaro623 <ryutarouonozawa@icloud.com>
2022-03-06 15:55:16 -0800

    added details

--- Code/P3.java
+++ Code/P3.java
@@ -1,5 +1,8 @@
 package Code;
-
+/**Problem 3
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
 public class P3 {
     public static void main(String[] args) {
         P3 h = new P3();
